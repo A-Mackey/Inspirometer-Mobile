58e7ba31a6313d41b29efeb121442aee2b5a736c
Aidan Mackey <57772122+A-Mackey@users.noreply.github.com>
2020-10-11 17:23:32 -0700

    Update to the theme colors

--- Inspirometer-Mobile/App.js
+++ Inspirometer-Mobile/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import {StyleSheet} from 'react-native'
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 import { Ionicons, AntDesign, FontAwesome } from '@expo/vector-icons';
-import { Container, Header, View, Fab, Button, Icon, Body, Title, Tabs, Tab, ScrollableTab, Footer, Content, FooterTab } from 'native-base';
+import { Container, Header, View, Fab, Button, Icon, Body, Title, Tabs, Tab, ScrollableTab, Footer, Content, FooterTab, Left, Right } from 'native-base';
 import Chart from './pages/Chart'
 import NewReading from './pages/NewReading'
 import Settings from './pages/Settings'
@@ -28,11 +29,7 @@ export default class App extends React.Component {
       ...Ionicons.font,
     });
     this.setState({ isReady: true });
-  }
-
-  testMethod() {
-    console.log("test");
-  }
+  };
 
   render() {
     if (!this.state.isReady) {
@@ -51,33 +48,103 @@ export default class App extends React.Component {
       AppComponent = User
     }
 
+    const iconFooterSize = 28;
+
+    //FUNCTIONS
+  
+    function testFunction(test) {
+      console.log("Redoing Test...");
+    }
+
+    //STYLESHEET
+
+    const styles = StyleSheet.create({
+      header: {
+        backgroundColor: '#1D3461',
+        textShadowColor: 'white',
+      },
+
+      headerText: {
+        color: '#FFF',
+      },
+
+      headerIcons: {
+        color: '#FFF',
+      },  
+
+      body: {
+        backgroundColor: 'white',
+      },
+
+      footer: {
+        backgroundColor: '#1D3461',
+      },
+
+      footerIconActive: {
+        color: '#000',
+      },
+
+      footerIconDeactive: {
+        color: '#FFF',
+      }
+    });
+
+    //MAIN BODY
+
     return (
       <Container>
-        <Header>
+        <Header style={styles.header}>
+          <Left>
+
+          </Left>
+
           <Body>
-              <Title>Inspirometer</Title>
+              <Title style={styles.headerText}>Inspirometer</Title>
           </Body>
+
+          <Right>
+            <Button transparent onPress={testFunction}>
+              <Icon name='refresh' style={styles.headerIcons}></Icon>
+            </Button>
+          </Right>
         </Header>
+
+
         <Content>
           <AppComponent/>
         </Content>
-        <Footer>
+
+
+        <Footer style={styles.footer}>
           <FooterTab>
             <Button active={this.state.index === 0} onPress={() => this.switchScreen(0)}>
-              <AntDesign name="edit" size={28} />
+              <AntDesign style={this.state.index == 0 ? styles.footerIconActive : styles.footerIconDeactive} name="edit" size={iconFooterSize} />
             </Button>
+            </FooterTab>
+
+            <FooterTab>
             <Button active={this.state.index === 1} onPress={() => this.switchScreen(1)}>
-              <AntDesign name="linechart" size={28} />
+              <AntDesign style={this.state.index == 1 ? styles.footerIconActive : styles.footerIconDeactive} name="linechart" size={iconFooterSize} />
             </Button>
+            </FooterTab>
+
+            <FooterTab>
             <Button active={this.state.index === 2} onPress={() => this.switchScreen(2)}>
-              <FontAwesome name="gear" size={28} />
+              <FontAwesome style={this.state.index == 2 ? styles.footerIconActive : styles.footerIconDeactive} name="gear" size={iconFooterSize} />
             </Button>
+            </FooterTab>
+
+            <FooterTab>
             <Button active={this.state.index === 3} onPress={() => this.switchScreen(3)}>
-              <FontAwesome name="user" size={28} />
+              <FontAwesome style={this.state.index == 3 ? styles.footerIconActive : styles.footerIconDeactive} name="user" size={iconFooterSize} />
             </Button>
           </FooterTab>
+
         </Footer>
+
+        
       </Container>
     );
   }
+
 }
--- Inspirometer-Mobile/pages/Settings.js
+++ Inspirometer-Mobile/pages/Settings.js
@@ -4,7 +4,6 @@ export default class ButtonExample extends Component {
   render() {
     return (
       <Container>
-        <Header />
         <Content>
             <Text>Settings</Text>
         </Content>
